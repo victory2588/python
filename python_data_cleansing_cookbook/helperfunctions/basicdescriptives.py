518463c7cc2535cb6b7f70d99c0edf9375e3228c
Yewon Kim <victory2588@naver.com>
2023-05-17 16:01:19 +0900

    데이터를 처음 살펴보는 함수

--- python_data_cleansing_cookbook/helperfunctions/basicdescriptives.py
+++ python_data_cleansing_cookbook/helperfunctions/basicdescriptives.py
@@ -9,9 +9,25 @@ def getfirstlook(df,nrows=5,uniqueids=None):
     out['index']=df.index
 
     if (uniqueids is not None):
-        out['uniqueids']=df[uniqueids].nuique()
+        out['uniqueids']=df[uniqueids].nunique()
     return out
 
 def displaydict(dicttodisplay):
     print(*(': '.join(map(str,x))
             for x in dicttodisplay.items()), sep='\n\n')
+    
+def gettots(df):
+    out={}
+
+    out['min']=df.min()
+    out['per15']=df.quantile(0.15)
+    out['qr1']=df.quantile(0.25)
+    out['med']=df.median()
+    out['qr3']=df.quantile(0.75)
+    out['per85']=df.quantile(0.85)
+    out['max']=df.max()
+    out['count']=df.count()
+    out['mean']=df.mean()
+    out['iqr']=out['qr3'] - out['qr1']
+
+    return pd.DataFrame(out)
